15b468e5b731100ca3c021f1fd31cd057ec1c351
CarlHagred <carl.hagred@gmail.com>
2020-12-17 20:26:02 +0100

    Fixat type och färger

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -57,9 +57,30 @@ let statsNameTwo = 'Stats';
 let chartColorTwo = '';
 
 //CSS variables
-var root = document.querySelector(':root')
+const root = document.querySelector(':root')
 
-//Load Pokemons to Graph        
+//ElementColors
+const elementColors = {
+    'fire': 'rgba(255, 90, 0, 0.7)',
+    'grass': 'rgba(0, 186, 0, 0.7)',
+    'water': 'rgba(0, 168, 224, 0.7)',
+    'bug': 'rgba(177, 207, 0, 0.7)',
+    'normal':'rgba(243, 245, 246, 0.7)',
+    'electric': 'rgba(255, 245, 0, 0.7)',
+    'ground':'rgba(255, 192, 0, 0.7)',
+    'psychic': 'rgba(255, 57, 196, 0.7)',
+    'ghost': 'rgba(53, 0, 193, 0.7)',
+    'fairy': 'rgba(255, 126, 193, 0.7)',
+    'dragon': 'rgba(0, 0, 255, 0.7)',
+    'steel': 'rgba(180, 180, 180, 0.7)',
+    'ice': 'rgba(0, 255, 255, 0.7)',
+    'poison': 'rgba(136, 0, 135, 0.7)',
+    'flying': 'rgba(73, 128, 255, 0.7)',
+    'fighting': 'rgba(162, 15, 0, 0.7)',
+    'rock': 'rgba(162, 113, 0, 0.7)',
+}
+
+//Load Pokemons to lists        
 $(document).ready(function () { 
     $.ajax({
         url: "https://pokeapi.co/api/v2/pokemon?limit=151",
@@ -69,7 +90,6 @@ $(document).ready(function () {
         for(i = 0; i < data['results'].length; i++){
             pokemonArray.push(data['results'][i]);
             document.getElementById('pokemonList').innerHTML += '<a href="#" class="dropdown-item" id="' + i + '" onclick="pokemonSelectFunc(this.id, 1)">' + capitalizeFirstLetter(data['results'][i]['name']) + '</a>';
-
 
             document.getElementById('pokemonListTwo').innerHTML += '<a href="#" class="dropdown-itemTwo" id="' + i + '" onclick="pokemonSelectFunc(this.id, 2)">' + capitalizeFirstLetter(data['results'][i]['name']) + '</a>';
         }
@@ -153,6 +173,7 @@ function chartTwo(){
     });
 }
 
+//Gör apianrop på en specefik pokeon och sätter dess info till sina html-taggar
 function pokemonSelectFunc(id, nr){
     const details = pokemonArray[id]['url'];
     
@@ -172,35 +193,40 @@ function pokemonSelectFunc(id, nr){
                 chartLabels.push(capitalizeFirstLetter(data["stats"][c]["stat"]["name"]));
                 chartContent.push(data["stats"][c]["base_stat"]);
             }
-
             chart();
-
-            /*
-            chartContent.push(data["height"]);
-            chartContent.push(data["weight"]);
-            */
             
             //PokemonInfo 
             $('#name').text(capitalizeFirstLetter(pokemonArray[id]['name']));
             $('#image').html('<img src="' + data['sprites']['front_default'] + '">');
+            $('.typeHolder').html('<p class="type" style="background-color: '+ elementColors[data["types"][0]["type"]["name"]] +';">' + data["types"][0]["type"]["name"] + '</p>');
+            if(data["types"][1]["type"]["name"] !== undefined){
+                $('.typeHolder').html(
+                    '<p class="type" style="background-color: '+ elementColors[data["types"][0]["type"]["name"]] +';">' + data["types"][0]["type"]["name"] + '</p> \n' +
+                    '<p class="type" style="background-color: '+ elementColors[data["types"][1]["type"]["name"]] +';">' + data["types"][1]["type"]["name"] + '</p>'
+                );
+            }
             $('#atack1').text(capitalizeFirstLetter(data['moves'][0]['move']['name']));
             $('#atack2').text(capitalizeFirstLetter(data['moves'][1]['move']['name']));
             $('#atack3').text(capitalizeFirstLetter(data['moves'][2]['move']['name']));
             $('#atack4').text(capitalizeFirstLetter(data['moves'][3]['move']['name']));
         }else{
-            
             chartLabelsTwo = [];
             chartContentTwo = [];
             for(let c = 0; c < 5; c++){
                 chartLabelsTwo.push(capitalizeFirstLetter(data["stats"][c]["stat"]["name"]));
                 chartContentTwo.push(data["stats"][c]["base_stat"]);
             }
-
             chartTwo();
             
-
             $('#nameTwo').text(capitalizeFirstLetter(pokemonArray[id]['name']));
             $('#imageTwo').html('<img src="' + data['sprites']['front_default'] + '">');
+            $('.typeHolderTwo').html('<p class="type" style="background-color: '+ elementColors[data["types"][0]["type"]["name"]] +';">' + data["types"][0]["type"]["name"] + '</p>');
+            if(data["types"][1]["type"]["name"] !== undefined){
+                $('.typeHolderTwo').html(
+                    '<p class="type" style="background-color: '+ elementColors[data["types"][0]["type"]["name"]] +';">' + data["types"][0]["type"]["name"] + '</p> \n' +
+                    '<p class="type" style="background-color: '+ elementColors[data["types"][1]["type"]["name"]] +';">' + data["types"][1]["type"]["name"] + '</p>'
+                );
+            }
             $('#atack1Two').text(capitalizeFirstLetter(data['moves'][0]['move']['name']));
             $('#atack2Two').text(capitalizeFirstLetter(data['moves'][1]['move']['name']));
             $('#atack3Two').text(capitalizeFirstLetter(data['moves'][2]['move']['name']));
@@ -209,121 +235,22 @@ function pokemonSelectFunc(id, nr){
     });
 }
 
+//Sätter första bokstaven till stor bokstav
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-//gör funktion som ändrar färg baserat på element av pokemon
+//Ändrar färg baserat på element av pokemon
 function setChartColor(type, nr){
     console.log(type);
     if(nr === 1){
-        if(type === 'fire'){
-            chartColor = 'rgba(255, 90, 0, 0.7)';
-            chart();
-        } else if (type === 'grass'){
-            chartColor = 'rgba(0, 186, 0, 0.7)';
-            chart();
-        } else if (type === 'water'){
-            chartColor = 'rgba(0, 168, 224, 0.7)';
-            chart();
-        } else if (type === 'bug'){
-            chartColor = 'rgba(177, 207, 0, 0.7)';
-            chart();
-        } else if (type === 'normal'){
-            chartColor = 'rgba(243, 245, 246, 0.7)';
-            chart();
-        } else if (type === 'electric'){
-            chartColor = 'rgba(255, 245, 0, 0.7)';
-            chart();
-        } else if (type === 'ground'){
-            chartColor = 'rgba(255, 192, 0, 0.7)';
-            chart();
-        } else if (type === 'psychic'){
-            chartColor = 'rgba(255, 57, 196, 0.7)';
-            chart();
-        } else if (type === 'ghost'){
-            chartColor = 'rgba(53, 0, 193, 0.7)';
-            chart(); 
-        } else if (type === 'fairy'){
-            chartColor = 'rgba(255, 126, 193, 0.7)';
-            chart();
-        } else if (type === 'dragon'){
-            chartColor = 'rgba(0, 0, 255, 0.7)';
-            chart();
-        } else if (type === 'steel'){
-            chartColor = 'rgba(180, 180, 180, 0.7)';
-            chart();
-        } else if (type === 'ice'){
-            chartColor = 'rgba(0, 255, 255, 0.7)';
-            chart();
-        } else if (type === 'poison'){
-            chartColor = 'rgba(136, 0, 135, 0.7)';
-            chart();
-        } else if (type === 'flying'){
-            chartColor = 'rgba(73, 128, 255, 0.7)';
-            chart();
-        } else if (type === 'fighting'){
-            chartColor = 'rgba(162, 15, 0, 0.7)';
-            chart();
-        }else if (type === 'rock'){
-            chartColor = 'rgba(162, 113, 0, 0.7)';
-            chart();
-        }
+        chartColor = elementColors[type];
+        chart();
         root.style.setProperty('--allShadow', '0px 6px 15px 0px ' + chartColor);
         root.style.setProperty('--typeColor', chartColor);
     } else {
-        if(type === 'fire'){
-            chartColorTwo = 'rgba(255, 90, 0, 0.7)';
-            chartTwo();
-        } else if (type === 'grass'){
-            chartColorTwo = 'rgba(0, 186, 0, 0.7)';
-            chartTwo();
-        } else if (type === 'water'){
-            chartColorTwo = 'rgba(0, 168, 224, 0.7)';
-            chartTwo();
-        } else if (type === 'bug'){
-            chartColorTwo = 'rgba(177, 207, 0, 0.7)';
-            chartTwo();
-        } else if (type === 'normal'){
-            chartColorTwo = 'rgba(243, 245, 246, 0.7)';
-            chartTwo();
-        } else if (type === 'electric'){
-            chartColorTwo = 'rgba(255, 245, 0, 0.7)';
-            chartTwo();
-        } else if (type === 'ground'){
-            chartColorTwo = 'rgba(255, 192, 0, 0.7)';
-            chartTwo();
-        } else if (type === 'psychic'){
-            chartColorTwo = 'rgba(255, 57, 196, 0.7)';
-            chartTwo();
-        } else if (type === 'ghost'){
-            chartColorTwo = 'rgba(53, 0, 193, 0.7)';
-            chartTwo();
-        } else if (type === 'fairy'){
-            chartColorTwo = 'rgba(255, 126, 193, 0.7)';
-            chartTwo();
-        } else if (type === 'dragon'){
-            chartColorTwo = 'rgba(0, 0, 255, 0.7)';
-            chartTwo();
-        } else if (type === 'steel'){
-            chartColorTwo = 'rgba(180, 180, 180, 0.7)';
-            chartTwo();
-        } else if (type === 'ice'){
-            chartColorTwo = 'rgba(0, 255, 255, 0.7)';
-            chartTwo();
-        } else if (type === 'poison'){
-            chartColorTwo = 'rgba(136, 0, 135, 0.7)';
-            chartTwo();
-        } else if (type === 'flying'){
-            chartColorTwo = 'rgba(73, 128, 255, 0.7)';
-            chartTwo();
-        } else if (type === 'fighting'){
-            chartColorTwo = 'rgba(162, 15, 0, 0.7)';
-            chartTwo();
-        }else if (type === 'rock'){
-            chartColor = 'rgba(162, 113, 0, 0.7)';
-            chart();
-        }
+        chartColorTwo = elementColors[type];
+        chartTwo();
         root.style.setProperty('--allShadowTwo', '0px 6px 15px 0px ' + chartColorTwo);
         root.style.setProperty('--typeColorTwo', chartColorTwo);
     }
