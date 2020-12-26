dff01e5d7bd6d0d2e463ee01f1b75cd47c30fe2a
CarlHagred <carl.hagred@gmail.com>
2020-12-26 12:51:28 +0100

    Påbörjat lol api

--- public/scripts/lol.js
+++ public/scripts/lol.js
@@ -1,12 +1,73 @@
-const banner = document.getElementById('banner');
+const banner = document.getElementById("banner");
 
-
-banner.addEventListener('click', async () => {
-    console.log('yolo');
+banner.addEventListener("click", async () => {
+    console.log("yolo");
 
     const api_url = `/lol`;
     const response = await fetch(api_url);
     const json = await response.json();
     console.log(json);
 });
+/*
+$(document).ready(function () {
+    $.ajax({
+        url:
+            "https://ddragon.leagueoflegends.com/cdn/9.18.1/data/en_US/champion.json",
+        headers: { Accept: "application/json" },
+    }).done(function (data) {
+        console.log(data);
+    });
+});
+*/
 
+//Load Pokemons to lists
+$(document).ready(function () {
+    $.ajax({
+        url:
+            "https://ddragon.leagueoflegends.com/cdn/9.18.1/data/en_US/champion.json",
+        headers: { Accept: "application/json" },
+    }).done(function (data) {
+        console.log(data);
+        let i = 0;
+        for (const [key, value] of Object.entries(data.data)) {
+            console.log(`${key}`);
+
+            document.getElementById("pokemonList").innerHTML +=
+                '<a href="#" class="dropdown-item" id="' +
+                i +
+                '" onclick="pokemonSelectFunc(this.id, 1)">' +
+                key +
+                "</a>";
+
+            document.getElementById("pokemonListTwo").innerHTML +=
+                '<a href="#" class="dropdown-itemTwo" id="' +
+                i +
+                '" onclick="pokemonSelectFunc(this.id, 2)">' +
+                key +
+                "</a>";
+            i++;
+        }
+        /*
+        for (i = 0; i < Object.keys(data.data).length; i++) {
+            console.log(i);
+            console.log(data.data[i].id);
+            /*
+            pokemonArray.push(data["results"][i]);
+            document.getElementById("pokemonList").innerHTML +=
+                '<a href="#" class="dropdown-item" id="' +
+                i +
+                '" onclick="pokemonSelectFunc(this.id, 1)">' +
+                capitalizeFirstLetter(data["results"][i]["name"]) +
+                "</a>";
+
+            document.getElementById("pokemonListTwo").innerHTML +=
+                '<a href="#" class="dropdown-itemTwo" id="' +
+                i +
+                '" onclick="pokemonSelectFunc(this.id, 2)">' +
+                capitalizeFirstLetter(data["results"][i]["name"]) +
+                "</a>";
+                
+        }
+        */
+    });
+});
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-app.listen(port, () => console.log('listening att ' + port));
-app.use(express.static('public'));
+app.listen(port, () => console.log("listening att " + port));
+app.use(express.static("public"));
 
-app.get('/lol', async (request, response) => {
-    const api_url = "https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Darius?api_key=RGAPI-505e855c-eb51-4456-824d-653b042f5e53";
-    const fetch_response = await fetch (api_url);
+app.get("/lol", async (request, response) => {
+    const api_url =
+        "https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Darius?api_key=RGAPI-eb7c8129-7d81-431a-9010-53c64dc1f844";
+    const fetch_response = await fetch(api_url);
     const json = await fetch_response.json();
     response.json(json);
-})
+});
